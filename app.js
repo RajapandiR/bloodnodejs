2cabcbff98cb7e121f577f95b013002a0e529505
RajapandiR <rajapandibsc12@gmail.com>
2020-07-27 12:16:50 +0530

    bug fixing

--- app.js
+++ app.js
@@ -13,14 +13,17 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', routers)
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
 const NODE_ENV = process.env.NODE_ENV || 'local';
-mongoose.connect(config[NODE_ENV].MONGO_DB_URI, 
-	{
+mongoose.connect(config[NODE_ENV].MONGO_DB_URI,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex:true,
+    useUnifiedTopology: true
   });
 const db = mongoose.connection;
+
 db.on('error', (err) => {
   console.log(err);
 });
