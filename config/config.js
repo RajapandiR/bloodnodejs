22115a5eb67985ecea9df98f5f9cd0342124fd47
RajapandiR <rajapandibsc12@gmail.com>
2020-07-27 11:54:40 +0530

    bug fixing

---
+++ config/config.js
@@ -0,0 +1,10 @@
+export default {
+  PORT: 300,
+  SECRET: 'ATSSecretKey',
+   local: {
+    DB: 'mongodb://localhost:27017/blood'
+  },
+  dev: {
+    DB: 'mongodb://localhost:27017/blood'
+  },
+}
