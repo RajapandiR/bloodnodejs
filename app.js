87241923e1f28101857b0e37b5b3b449e9f493d3
RajapandiR <rajapandibsc12@gmail.com>
2020-07-27 12:21:24 +0530

    bug fixing

--- app.js
+++ app.js
@@ -13,14 +13,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', routers)
-mongoose.set('useCreateIndex', true);
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-const NODE_ENV = process.env.NODE_ENV || 'local';
-mongoose.connect(config[NODE_ENV].MONGO_DB_URI,
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useFindAndModify', false);
+// const NODE_ENV = process.env.NODE_ENV || 'local';
+mongoose.connect(config.MONGO_DB_URI,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
   });
 const db = mongoose.connection;
 
--- config/config.js
+++ config/config.js
@@ -1,10 +1,6 @@
 export default {
   PORT: 300,
   SECRET: '1234567890',
-   local: {
-    MONGO_DB_URI: 'mongodb://localhost:27017/blood'
-  },
-  dev: {
-    MONGO_DB_URI: 'mongodb://localhost:27017/blood'
-  },
+  MONGO_DB_URI: 'mongodb://localhost:27017/blood'
+  
 }
