b076d07aad9e2815eae44fa9ca2893b89c12bcd1
RajapandiR <rajapandibsc12@gmail.com>
2020-07-31 11:12:05 +0530

    Change mongodb url

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/', routers)
 // mongoose.set('useNewUrlParser', true);
 // mongoose.set('useFindAndModify', false);
 const NODE_ENV = process.env.NODE_ENV || 'local';
-mongoose.connect(config[NODE_ENV].MONGODB_URI,
+mongoose.connect(process.env.MONGODB_URI || config.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ export default {
   PORT: 3000,
   SECRET: '1234567890',
   // MONGO_DB_URI: 'mongodb://localhost:27017/bloods'
-  local: {
-    MONGODB_URI: 'mongodb://localhost:27017/bloods'
-  },
-  dev: {
-    MONGODB_URI: 'mongodb://localhost:27017/blood'
-  }
+  // local: {
+  //   MONGODB_URI: 'mongodb://localhost:27017/bloods'
+  // },
+  // dev: {
+  //   MONGODB_URI: 'mongodb://localhost:27017/blood'
+  // }
 }
